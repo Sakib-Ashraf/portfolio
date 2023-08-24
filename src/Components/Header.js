13e3e6cb8b3d034c5f9f54eef1e9cf6a170c4ee2
SakibAshraf <59136817+Sakib-Ashraf@users.noreply.github.com>
2023-08-24 15:52:50 +0600

    updated portfolio

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -14,130 +14,20 @@ import {
 	faTwitter,
 	faWhatsapp,
 } from '@fortawesome/free-brands-svg-icons';
-import Particles from 'react-particles-js';
-
-const particlesOptions = {
-	particles: {
-		number: {
-			value: 60,
-			density: {
-				enable: true,
-				value_area: 800,
-			},
-		},
-		color: {
-			value: '#3269ff',
-		},
-		shape: {
-			type: 'circle',
-			stroke: {
-				width: 0,
-				color: '#3269ff',
-			},
-			polygon: {
-				nb_sides: 5,
-			},
-			image: {
-				src: 'img/github.svg',
-				width: 100,
-				height: 100,
-			},
-		},
-		opacity: {
-			value: 0.5,
-			random: false,
-			anim: {
-				enable: true,
-				speed: 0.1,
-				opacity_min: 0.1,
-				sync: true,
-			},
-		},
-		size: {
-			value: 3,
-			random: true,
-			anim: {
-				enable: true,
-				speed: 2,
-				size_min: 0.1,
-				sync: true,
-			},
-		},
-		line_linked: {
-			enable: true,
-			distance: 150,
-			color: '#3269ff',
-			opacity: 0.4,
-			width: 1,
-		},
-		move: {
-			enable: true,
-			speed: 1,
-			direction: 'none',
-			random: false,
-			straight: false,
-			out_mode: 'out',
-			bounce: false,
-			attract: {
-				enable: false,
-				rotateX: 600,
-				rotateY: 1200,
-			},
-		},
-	},
-	interactivity: {
-		detect_on: 'canvas',
-		events: {
-			onhover: {
-				enable: false,
-				mode: 'grab',
-			},
-			onclick: {
-				enable: true,
-				mode: 'push',
-			},
-			resize: true,
-		},
-		modes: {
-			grab: {
-				distance: 400,
-				line_linked: {
-					opacity: 1,
-				},
-			},
-			bubble: {
-				distance: 400,
-				size: 40,
-				duration: 2,
-				opacity: 8,
-				speed: 3,
-			},
-			repulse: {
-				distance: 200,
-				duration: 0.4,
-			},
-			push: {
-				particles_nb: 4,
-			},
-			remove: {
-				particles_nb: 2,
-			},
-		},
-	},
-	retina_detect: true,
-};
 
 
 class Header extends Component {
+	
   render() {
+	
     if (!this.props.data) return null;
 
     const name = this.props.data.name;
     const description = this.props.data.description;
 
 	  return (
-			<>
-				<Particles className='particles' params={particlesOptions} />
+			<div>
+				
 				<header id='home'>
 					<nav id='nav-wrap'>
 						<a
@@ -312,9 +202,8 @@ class Header extends Component {
 						</a>
 					</p>
 				</header>
-			</>
+			</div>
 		);
   }
 }
-
 export default Header;
